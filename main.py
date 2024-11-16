f26b82c858637ffcbc930fd16778223851be1860
nastts <159527618+nastts@users.noreply.github.com>
2024-11-16 11:06:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import telebot
+from model import get_class
+
+bot = telebot.TeleBot('8019455260:AAGZ1KjapSDkIQCb3y4tJN-CNFj4EQdULt8')
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, """привет!!!""")
+
+@bot.message_handler(content_types=['photo'])
+def handle_docs_photo(message):
+    if not message.photo:
+        return bot.send_message(message.chat.id, "нет картинки")
+    
+    file_info = bot.get_file(message.photo[-1].file_id)
+    file_name = file_info.file_path.split('/')[-1]
+
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    result = get_class(model_path="keras_model.h5", labels_path="labels.txt", image_path=file_name)
+    bot.send_message(message.chat.id, result)
+
+bot.polling()
